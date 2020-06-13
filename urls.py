5fee560abc17ef249fbb30c51edf940d8392fdaa
MPandey1998 <65955163+MPandey1998@users.noreply.github.com>
2020-06-13 15:05:54 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+urlpatterns = \
+    [
+    path('register',views.Register,name="User Register"),
+    path('login',views.Login,name="User Login"),
+    path('Forgotpassword',views.Forgotpassword,name="User Forgotpassword"),
+    path('Allplans',views.Allplans,name=" Allplans"),
+    path('Allcats',views.All_Intrest_Category,name=" All Cats"),
+    path('Allsubcats',views.All_Intrest_Sub_Category,name=" All Sub Cats"),
+    path('Setuserplan',views.Setuserplan,name=" Set user plan"),
+    path('Uploadvideos',views.Videos, name=" Upload videos"),
+    path('SpecificVideos',views.Videos_for_specific_Category,name=" Set user plan"),
+]
